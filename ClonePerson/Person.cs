dabcd7e95895c67646c9bb7a76ae7cf42d3ba82d
Dánjal Skaalum <danjal.skaalum@gmail.com>
2023-12-08 12:50:29 +0100

    Add project files.

---
+++ ClonePerson/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClonePerson
+{
+    internal class Person : ICloneable
+    {
+        private string _name;
+        private int _height;
+        private int _weight;
+        public string Name { get; set;}
+        public int Height { get; set; }
+        public int Weight { get; set; }
+
+        public Person()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\nHeight: {Height}\nWeight: {Weight}\n";
+        }
+
+        public object Clone()
+        {
+
+            return MemberwiseClone();
+        }
+    }
+}
---
+++ ClonePerson/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ClonePerson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new()
+            {
+                Name = "John Doe",
+                Height = 165,
+                Weight = 90
+            };
+            Person p2 = p1;
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+
+            Person p3 = (Person)p1.Clone();
+
+            Console.WriteLine(p3);
+        }
+    }
+}
